7caf34102554b0502649f458621c10a919988d93
RitikTailor15 <ritiktailor15@gmail.com>
2023-10-15 11:16:48 +0530

    Module Caching

---
+++ idea.js
@@ -0,0 +1,4 @@
+const ideaHero = require("./super-hero");
+console.log(ideaHero.getName());
+ideaHero.setName("IronMan");
+console.log(ideaHero.getName());
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-require("./batman");
-require("./superMan");
-require("./life");
+const superHero = require("./super-hero");
+console.log(superHero.getName());
+superHero.setName("SuperMan");
+console.log(superHero.getName());
+
+const superHero2 = require("./super-hero");
+console.log(superHero2.getName());
+
+require("./idea");
+
+// require("./batman");
+// require("./superMan");
+// require("./life");
 // const add = require("./add.js");
 
 // console.log("Hello World");
