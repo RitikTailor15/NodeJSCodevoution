2ce7769810591924a2118fe9f90af7da5fc3a4fd
RitikTailor15 <ritiktailor15@gmail.com>
2023-10-15 12:02:19 +0530

    Import and exports patterns in ES modules

---
+++ math-esm.mjs
@@ -0,0 +1,31 @@
+// ******** First Pattern ********
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// export default add;
+
+// ******** Second Pattern ********
+// export default (a, b) => {
+//   return a + b;
+// };
+
+// ******** Third Pattern ********
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// const subtract = (a, b) => {
+//   return a - b;
+// };
+
+// export default { add, subtract };
+
+// ******** Fourth Pattern ********
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const subtract = (a, b) => {
+  return a - b;
+};
