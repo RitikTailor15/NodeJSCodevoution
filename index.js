0c5695029b404e58b18f0ec363375b4b8c5d7220
RitikTailor15 <ritiktailor15@gmail.com>
2023-10-17 00:27:33 +0530

    Sync pbkdf

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+// ************* Thread pool with crypto ***************
+const crypto = require("node:crypto");
+
+const start = Date.now();
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+console.log("Hash:", Date.now() - start);
+// ***********************************
 // ************* Thread pool ***************
-const fs = require("node:fs");
-console.log("First");
-fs.readFile("./file.txt", "utf-8", (error, data) => {
-  console.log("File contents");
-});
-console.log("Last");
+// const fs = require("node:fs");
+// console.log("First");
+// fs.readFile("./file.txt", "utf-8", (error, data) => {
+//   console.log("File contents");
+// });
+// console.log("Last");
 // ***********************************
 // ************* HTTP routing ***************
 // const http = require("node:http");
