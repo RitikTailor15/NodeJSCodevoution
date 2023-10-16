fe1647793a9d81a3cac34f61f5c2479a49e9fec2
RitikTailor15 <ritiktailor15@gmail.com>
2023-10-16 08:24:31 +0530

    JSON Response

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 const http = require("node:http");
 
 const server = http.createServer((req, res) => {
-  console.log(req, "req");
-  res.writeHead(200, { "Content-type": "text/plain" });
-  res.end("Hello world");
+  const superHero = {
+    firstName: "Ritik",
+    lastName: "Tailor",
+  };
+  res.writeHead(200, { "Content-type": "application/json" });
+  res.end(JSON.stringify(superHero));
 });
 
 server.listen(3000, () => {
